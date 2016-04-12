a344900b8786f82a30008b57aaaa0912d3f53123
Leilani Ann Raranga <retrosole@gmail.com>
2016-04-12 13:43:13 -0700

    precompile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724203938) do
+ActiveRecord::Schema.define(version: 20160304163608) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "amenities"
+    t.integer  "place_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "categories", ["place_id"], name: "index_categories_on_place_id", using: :btree
+
+  create_table "gestures", force: true do |t|
+    t.text     "data",                   null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "name",       limit: nil
+  end
 
   create_table "groups", force: true do |t|
     t.string   "name"
@@ -21,7 +40,7 @@ ActiveRecord::Schema.define(version: 20150724203938) do
     t.datetime "updated_at"
   end
 
-  add_index "groups", ["member_id"], name: "index_groups_on_member_id"
+  add_index "groups", ["member_id"], name: "index_groups_on_member_id", using: :btree
 
   create_table "members", force: true do |t|
     t.string   "first_name"
@@ -39,7 +58,78 @@ ActiveRecord::Schema.define(version: 20150724203938) do
     t.datetime "updated_at"
   end
 
-  add_index "memberships", ["group_id"], name: "index_memberships_on_group_id"
-  add_index "memberships", ["member_id"], name: "index_memberships_on_member_id"
+  add_index "memberships", ["group_id"], name: "index_memberships_on_group_id", using: :btree
+  add_index "memberships", ["member_id"], name: "index_memberships_on_member_id", using: :btree
+
+  create_table "place_categories", force: true do |t|
+    t.integer  "place_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "place_categories", ["category_id"], name: "index_place_categories_on_category_id", using: :btree
+  add_index "place_categories", ["place_id"], name: "index_place_categories_on_place_id", using: :btree
+
+  create_table "place_types", force: true do |t|
+    t.integer  "type_id"
+    t.integer  "place_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "place_types", ["place_id"], name: "index_place_types_on_place_id", using: :btree
+  add_index "place_types", ["type_id"], name: "index_place_types_on_type_id", using: :btree
+
+  create_table "places", force: true do |t|
+    t.string   "name"
+    t.string   "location"
+    t.integer  "user_id"
+    t.integer  "type_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "places", ["category_id"], name: "index_places_on_category_id", using: :btree
+  add_index "places", ["type_id"], name: "index_places_on_type_id", using: :btree
+  add_index "places", ["user_id"], name: "index_places_on_user_id", using: :btree
+
+  create_table "playbacks", force: true do |t|
+    t.string   "name",       limit: nil
+    t.text     "data"
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "reviews", force: true do |t|
+    t.text     "description"
+    t.integer  "stars"
+    t.integer  "user_id"
+    t.integer  "place_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "reviews", ["place_id"], name: "index_reviews_on_place_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
+
+  create_table "types", force: true do |t|
+    t.string   "gender"
+    t.integer  "place_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "types", ["place_id"], name: "index_types_on_place_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "handle"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
